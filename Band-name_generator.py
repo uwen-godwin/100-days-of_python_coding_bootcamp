710b1d176d4fce8f72702a61dfaa69eccebece0d
uwen-godwin <uwengodwin001@gmail.com>
2023-10-07 22:48:09 +0100

    Band name generator

---
+++ Band-name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to our band name generator\n")
+city = input("What city did you grop up in?\n")
+pet = input("What is your pet name?\n")
+print("Your band name could be " + city +  " " + pet)
