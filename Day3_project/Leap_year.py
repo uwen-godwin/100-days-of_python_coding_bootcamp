6d459c1b7d0c87f34def2a6d67d5f2eb5bc8bba3
uwen-godwin <uwengodwin001@gmail.com>
2023-11-21 17:45:06 +0100

    Leap year

---
+++ Day3_project/Leap_year.py
@@ -0,0 +1,19 @@
+#  a program that works out whether if a given year is a leap year.
+# A normal year has 365 days, leap years have 366, with an extra day in February.
+# The reason why we have leap years is really fascinating, this video does it more justice.
+# Which year do you want to check?
+
+year = int(input())
+
+if year % 4 == 0:
+  if year % 100 == 0:
+        # Not a leap year, unless special case
+    if year % 400 == 0:
+      print("leap year")
+    else:
+      print("Not leap year")
+  else:
+    print("Leap year")
+else:
+  print("not leap year")
+
