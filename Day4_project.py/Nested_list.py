80859b984baaa858a8d4797cb79773f5a634e54e
uwen-godwin <uwengodwin001@gmail.com>
2023-11-25 16:00:55 +0100

    updated

---
+++ Day4_project.py/Nested_list.py
@@ -0,0 +1,10 @@
+# Two created list with different items
+
+size_shirt = ["Small", "Medium", "Large", "Extra large"]
+size_shoes = ["10", "20", "25", "30", "35", "40", "45"]
+
+# Nested list
+two_list = [size_shirt, size_shoes]
+
+# print all the items in the list
+print(two_list)
--- Day4_project.py/list_datastructure.py
+++ Day4_project.py/list_datastructure.py
@@ -5,5 +5,8 @@ print(len(government)) # print the lenght of the list
 
 government.extend(["calabar", "Abi"]) # Add two items to the list.
 
-print(government) # Print the new list
+print(government[6]) # Print the item of the list at index 6 which is Abi.
+
+print(government) # print all the items in the new list.
+
 print(len(government)) # Check the list lenght again
