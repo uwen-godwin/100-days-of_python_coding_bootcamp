4e5858a3e7030282a60dfa316e2303e255350ea4
uwen-godwin <uwengodwin001@gmail.com>
2023-11-27 03:16:53 +0100

    Treasure map project

---
+++ Day4_project.py/treasure_map.py
@@ -0,0 +1,19 @@
+# A program that will mark a spot on a map with an X.
+# In the starting code, you will find a variable called map.
+# This map contains a nested list
+
+line1 = ["⬜", "⬜", "⬜"]
+line2 = ["⬜", "⬜", "⬜"]
+line3 = ["⬜", "⬜", "⬜"]
+
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
+
+print(f"{line1}\n{line2}\n{line3}")
